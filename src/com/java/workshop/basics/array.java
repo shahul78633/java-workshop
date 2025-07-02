40c220ba5b6fbf2aefdfafc0dcab6c9a7eb828b1
shahul78633 <shahulhamedkhanm@gmail.com>
2025-07-02 14:47:34 +0530

    new file

---
+++ src/com/java/workshop/basics/array.java
@@ -0,0 +1,10 @@
+public class array {
+    
+    public static void main(String[] args){
+        int[]marks = {20,45,67,87,45,34};
+        for (int mark:marks){
+            System.out.println(mark);
+            
+        }
+    }
+}
