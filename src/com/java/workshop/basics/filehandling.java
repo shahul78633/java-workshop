f419683e19d47454b728f01564cc09240733bb36
shahul78633 <shahulhamedkhanm@gmail.com>
2025-07-08 15:52:28 +0530

    filehandilng

---
+++ src/com/java/workshop/basics/filehandling.java
@@ -0,0 +1,27 @@
+package com.java.workshop.basics;
+import java.io.File;
+import java.io.IOException;
+
+public class filehandling {
+    public static void main(String[] args){
+        File file = new File("src");
+        if (file.exists()) {
+            System.out.println("File exists: " + file.getAbsolutePath());
+        } else {
+            System.out.println("File does not exist.");
+        }
+
+        if(file.isDirectory()) {
+            System.out.println("It is a directory.");
+       File[] files = file.listFiles();
+            if (files != null) {
+                for (File f : files) {
+                    System.out.println("File: " + f.getName());
+                }
+            }
+        } else {
+            System.out.println("It is not a directory.");
+        }
+    }   
+}
+    
