0af2ff83f6fc8b100de3dd7351a52853fd960732
shahul78633 <shahulhamedkhanm@gmail.com>
2025-07-10 16:00:02 +0530

    updated

---
+++ src/main/java/com/java/workshop/collections/StudentMap.java
@@ -0,0 +1,22 @@
+package com.java.workshop.collections;
+
+import java.util.Map;
+
+public class StudentMap {
+    public static void main(String[] args){
+        Map<Integer, String> students = new HashMap<>();
+        students.put(101, "shahul");
+        students.put(102, "reshma");
+        students.put(103, "iman");
+        students.put(104, "zameer");
+        students.put(105, "santhosh");
+        students.put(106, "nowfiya");
+        students.put(107, "logesh");
+
+        System.out.println(students);
+        System.out.println(students.keySet());
+        System.out.println(students.values());
+        }
+    }
+    
+
